c2fadbb68f88ada7f9c58de4558275cc6bb8d366
ushisantoasobu <babblemann.shunsee@gmail.com>
2019-08-02 10:15:05 +0900

    Initial Commit

---
+++ researchRxTestFailed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  researchRxTestFailed
+//
+//  Created by shunsuke.sato on 2019/08/02.
+//  Copyright © 2019 shunsuke.sato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ researchRxTestFailedTests/researchRxTestFailedTests.swift
@@ -0,0 +1,34 @@
+//
+//  researchRxTestFailedTests.swift
+//  researchRxTestFailedTests
+//
+//  Created by shunsuke.sato on 2019/08/02.
+//  Copyright © 2019 shunsuke.sato. All rights reserved.
+//
+
+import XCTest
+@testable import researchRxTestFailed
+
+class researchRxTestFailedTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
